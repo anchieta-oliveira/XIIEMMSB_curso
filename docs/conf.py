0f969949291f880fa048c17a04e6c92c430e9868
Anchieta Oliveira <anchieta-13@hotmail.com>
2026-08-19 18:17:09 -0300

    temas novo

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,6 @@ language = "pt_BR"
 
 extensions = ["myst_parser"]
 
-html_theme = "furo"
-
-html_theme_options = {
-    "navigation_with_keys": True,
-}
+html_theme = "renku"
 
 html_title = "XII EMMSB 2026 — Energia Livre de Ligação"
